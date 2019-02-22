58fbf67fe43858dcb8ec28584a94951d3975221c
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2019-02-23 01:25:59 +1300

    change default layout to use bootstrap and add of a navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,27 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :challenges
+  root 'default#index'
+
+  resources :users do
+    member do
+      get :delete
+    end
+  end
+
+  resources :challenges do
+    member do
+      get :delete
+    end
+  end
+
+  get 'user/login', as: 'login'
+
   get 'challenge/index'
   get 'challenge/show'
   get 'challenge/edit'
   get 'challenge/delete'
   root 'default#index'
   
-  get 'default/index'
+  get 'default/index', as: 'homepage'
   get 'default/contact'
   get 'default/sandbox'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
